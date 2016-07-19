9a7112949f0927e57d62774a42ce6370df7c8426
shohan494 <shohan12224943.gmail.com>
2016-07-20 00:07:59 +0600

    created the CRUD functionality for the blog system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,35 @@ class ArticlesController < ApplicationController
     def new
     end
     
-    def create
-        render plain: params[:article].inspect
+    def index
+        @articles = Article.all
     end
+    
+    def create
+        
+        # render plain: params[:article].inspect
+        # @article = Article.new(params[:article])
+        # permission for title and text, it is needed for rails
+        # @article = Article.new(params.require(:article).permit(:title, :text))
+        # @article.save
+        # redirect_to @article
+  
+        @article = Article.new(article_params)
+ 
+        @article.save
+        redirect_to @article
+    end
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+ 
+    private
+        def article_params
+           params.require(:article).permit(:title, :text)
+        end
 end
+
+
+
+
