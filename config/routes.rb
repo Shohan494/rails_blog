502f0cf719909faa3e97f62222bf97272e944bba
shohan494 <shohan12224943.gmail.com>
2016-07-19 21:34:59 +0600

    edited .gitignore, created resourceful route in config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
- 
+  #get 'welcome/index'
+  
+  # declare a standard REST resource.
+  # it has defined routes for all the standard RESTful actions
+   resources :articles
   # this line tells that the root is now the created controller with the 'index' action
   root 'welcome#index'
 end
