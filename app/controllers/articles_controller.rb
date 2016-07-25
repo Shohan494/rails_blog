666e184f21f39c8fcde8d08d955d8e01965e49e0
shohan494 <shohan12224943.gmail.com>
2016-07-25 19:58:52 +0600

    added update button

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,27 +1,46 @@
 class ArticlesController < ApplicationController
     # The reason why we added @article = Article.new in the ArticlesController
-    # is that otherwise @article would be nil in our view, 
+    # is that otherwise @article would be nil in our view,
     # and calling @article.errors.any? would throw an error.
     def new
         @article = Article.new
     end
-    
+
     def index
         @articles = Article.all
     end
-    
+
+    def update
+        @article = Article.find(params[:id])
+
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    private
+        def article_params
+            params.require(:article).permit(:title, :text)
+        end
+
+
+
+
+
     def create
-        
+
         # render plain: params[:article].inspect
         # @article = Article.new(params[:article])
         # permission for title and text, it is needed for rails
         # @article = Article.new(params.require(:article).permit(:title, :text))
         # @article.save
         # redirect_to @article
-  
-   
+
+
         @article = Article.new(article_params)
- 
+
         if @article.save
             redirect_to @article
         else
@@ -30,14 +49,13 @@ class ArticlesController < ApplicationController
             render 'new'
         end
     end
-    
+
     def show
         @article = Article.find(params[:id])
     end
- 
+
     private
         def article_params
            params.require(:article).permit(:title, :text)
         end
 end
-
