c1a609be51dc4c64088024ba419f4aa0c964a071
shohan494 <shohan12224943.gmail.com>
2016-07-27 18:10:05 +0600

    completed the basic CRUD part

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,61 +1,49 @@
 class ArticlesController < ApplicationController
-    # The reason why we added @article = Article.new in the ArticlesController
-    # is that otherwise @article would be nil in our view,
-    # and calling @article.errors.any? would throw an error.
-    def new
-        @article = Article.new
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
     end
+  end
 
-    def index
-        @articles = Article.all
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
     end
+  end
 
-    def update
-        @article = Article.find(params[:id])
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
 
-        if @article.update(article_params)
-            redirect_to @article
-        else
-            render 'edit'
-        end
+    redirect_to articles_path
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
     end
-
-    private
-        def article_params
-            params.require(:article).permit(:title, :text)
-        end
-
-
-
-
-
-    def create
-
-        # render plain: params[:article].inspect
-        # @article = Article.new(params[:article])
-        # permission for title and text, it is needed for rails
-        # @article = Article.new(params.require(:article).permit(:title, :text))
-        # @article.save
-        # redirect_to @article
-
-
-        @article = Article.new(article_params)
-
-        if @article.save
-            redirect_to @article
-        else
-            # render method is used so that the @article object
-            # is passed back to the new template when it is rendered
-            render 'new'
-        end
-    end
-
-    def show
-        @article = Article.find(params[:id])
-    end
-
-    private
-        def article_params
-           params.require(:article).permit(:title, :text)
-        end
 end
