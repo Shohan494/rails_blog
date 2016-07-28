3c105927c1793de85496ceec167a816c47986ebb
shohan494 <shohan12224943.gmail.com>
2016-07-29 01:52:33 +0600

    created the Comment model, migrated db, configured route and created the controller also

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   #get 'welcome/index'
-  
+
   # declare a standard REST resource.
   # it has defined routes for all the standard RESTful actions
-   resources :articles
+   resources :articles do
+       resources :comments
+   end
   # this line tells that the root is now the created controller with the 'index' action
   root 'welcome#index'
 end
